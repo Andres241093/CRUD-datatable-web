d90bac8b31c760509d78eb854a6be5af0c0258b0
Andres <andresucero241093@gmail.com>
2020-10-14 01:55:08 -0430

    first route created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+{
+	path: 'home',
+	loadChildren: () => import('./datatable/datatable.module').then(m => m.DatatableModule)
+}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
